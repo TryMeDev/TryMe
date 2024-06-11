b2f4afe613a14fea08895d36da57ea0b05edd101
TryMeDev <contacttrymenow@gmail.com>
2024-06-11 22:52:26 +0800

    silly mistake?

--- AdminWeb/src/slices/apiSlice.ts
+++ AdminWeb/src/slices/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://trymeserver.onrender.com/",
+    baseUrl: "https://trymeserver-a52o.onrender.com/",
     prepareHeaders: (headers, { endpoint }) => {
       if (!headers.get("Authorization")) {
         const token = localStorage.getItem("token");
--- AudienceWeb/src/slices/apiSlice.ts
+++ AudienceWeb/src/slices/apiSlice.ts
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://trymeserver.onrender.com/",
-    mode: "no-cors",
+    baseUrl: "https://trymeserver-a52o.onrender.com/",
   }),
   reducerPath: "api",
   endpoints: (builder) => ({}),
