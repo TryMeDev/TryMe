bda393dda40a683a995b44675493c1abc84e784a
TryMeDev <contacttrymenow@gmail.com>
2024-06-13 20:42:05 +0800

    made dev env

--- AudienceWeb/src/slices/apiSlice.ts
+++ AudienceWeb/src/slices/apiSlice.ts
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { SERVER_URL } from "../config";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://trymeserver-a52o.onrender.com/",
+    baseUrl: SERVER_URL,
   }),
   reducerPath: "api",
   endpoints: (builder) => ({}),
--- PublisherWeb/src/config.ts
+++ PublisherWeb/src/config.ts
@@ -1,1 +1,2 @@
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
