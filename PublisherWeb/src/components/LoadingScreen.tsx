6527b60d075c7f201d3182ca755154b2d28529e2
TryMeDev <contacttrymenow@gmail.com>
2024-08-15 14:51:39 +0800

    tried making loading fullscreen

--- PublisherWeb/src/components/LoadingScreen.tsx
+++ PublisherWeb/src/components/LoadingScreen.tsx
@@ -7,10 +7,10 @@ const LoadingScreen: React.FC<{
 }> = ({ isLoading }) => {
   return (
     isLoading && (
-      <>
-        <ProgressSpinner className="absolute m-auto left-0 right-0 top-0 bottom-0" />
-        <BlockUI blocked fullScreen containerClassName="" />
-      </>
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <ProgressSpinner className="z-50" />
+        <BlockUI blocked fullScreen />
+      </div>
     )
   );
 };
